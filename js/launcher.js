d52d6d5d55acc4294726849adca411a2aff8efb2
Sergio Muriel <sergio.muriel@quiksilver.eu>
2016-12-05 18:51:43 +0100

    custom level load

--- js/launcher.js
+++ js/launcher.js
@@ -1,7 +1,15 @@
+var level = 1;
+var re;
+if(re = location.href.match(/level=(\d+)/))
+{
+    level=re[1];
+    console.log('level ',level);
+}
 var game = new Game({
     root: document.getElementById('game_container'),
     door_size: 30,
     debug_level: /debug/.test(location.href) ? 10 : 0,
+    level: level,
     enable_shadow: false
     /* 2 = debug enter/maze paths */
     /* 10 = debug walls */
