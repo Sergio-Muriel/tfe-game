98aed1acb63cb7b9ee519b1066cff89f3b5e160e
Sergio Muriel <tfeserver@gmail.com>
2016-12-15 18:20:14 +0100

    character move logic

--- js/character.js
+++ js/character.js
@@ -1,45 +1,5 @@
 var Character = function(game, options)
 {
-    var self=this;
-    this.id=game.getNewId();
-
-    this.is_hoverable=true;
-    this.can_walk_through=true;
-    this.has_walk_through_callback=false;
-    this.is_static_collision=false;
-
-    this.options = options;
-    this.game=game;
-    this.move_speed= 0.3;
-    this.run_speed= 2.0;
-    this.check_vision_every= 80;
-    this.attack_range = 10;
-
-    this.hovered=false;
-    this.is_dead=false;
-
-    this.weapon_type='stick';
-    this.weapon_speed= 0.5;
-    this.weapon_range= 20;
-    this.weapon_attack_damage = 100;
-    this.weapon_hit_chance = 0.5;
-
-    this.weapon_defense = 0.1;
-
-    this.max_life=10;
-    this.life=this.max_life;
-
-    this.is_running= false;
-    this.running_timer= null;
-
-    this.move_action_weight=0;
-
-    this.is_moving=false;
-    this.in_cells=[];
-    this.vision_angle = 55;
-    this.vision_distance=game.opt.door_size*1.0;
-    this.ennemy_detection_distance = game.opt.door_size*2.0;
-
 };
 
 Character.prototype.build = function()
@@ -456,14 +416,9 @@ Character.prototype.moveTo= function(pos)
     move_step_x = speed * directionX;
     move_step_z = speed * directionZ;
 
-    this.move_step_vector_x = new THREE.Vector2();
-    this.move_step_vector_z = new THREE.Vector2();
-    this.move_step_vector_x.x = move_step_x;
-    this.move_step_vector_x.z=0;
-    this.move_step_vector_z.x=0;
-    this.move_step_vector_z.z = move_step_z;
+    this.move_step_vector = pos.clone();
+    this.move_step_vector.sub(this.container.position).normalize();
 
-    // Actually moving...
     this.move_destination = pos;
     if(this.move_action_weight!=1)
     {
@@ -509,40 +464,24 @@ Character.prototype.move_step= function()
         {
             if(this.has_vision)
             {
-                // Moving X restrictions
-                if(Math.abs(this.container.position.x - this.move_destination.x) > 1)
+                // Moving
+                if(Math.abs(this.container.position.x - this.move_destination.x) > 1 || Math.abs(this.container.position.z - this.move_destination.z) > 1)
                 {
-                    this.vision.position.add(this.move_step_vector_x);
+                    this.vision.position.add(this.move_step_vector);
                 }
                 else
                 {
-                    this.vision.position.x = this.move_destination.x;
-                }
-                if(Math.abs(this.container.position.z - this.move_destination.z) > 1)
-                {
-                    this.vision.position.add(this.move_step_vector_z);
-                }
-                else
-                {
-                    this.vision.position.z = this.move_destination.z;
+                    this.vision.position = this.move_destination;
                 }
             }
             // Moving X restrictions
-            if(Math.abs(this.container.position.x - this.move_destination.x) > 1)
+            if(Math.abs(this.container.position.x - this.move_destination.x) > 1 || Math.abs(this.container.position.z - this.move_destination.z) > 1)
             {
-                this.container.position.add(this.move_step_vector_x);
+                this.container.position.add(this.move_step_vector);
             }
             else
             {
-                this.container.position.x = this.move_destination.x;
-            }
-            if(Math.abs(this.container.position.z - this.move_destination.z) > 1)
-            {
-                this.container.position.add(this.move_step_vector_z);
-            }
-            else
-            {
-                this.container.position.z = this.move_destination.z;
+                this.container.position = this.move_destination;
             }
         }
         else if(!this.friend && !game.focus_perso.is_dying)
