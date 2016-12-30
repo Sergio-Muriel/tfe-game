1d523ee78b670e47c1d87f502b48ca60e0bb06c0
Sergio Muriel <tfeserver@gmail.com>
2016-12-30 12:39:00 +0100

    x

--- js/game.js
+++ js/game.js
@@ -634,7 +634,7 @@ var Game = function(opt)
         switch(x)
         {
             case 0: this.zoomLevel({level:50, angle:-90, distance:50, time: 2}); break;
-            case 1: this.zoomLevel({level:100, angle:-90, distance:50, time: 2}); break;
+            case 1: this.zoomLevel({level:70, angle:-90, distance:100, time: 2}); break;
             case 2: this.zoomLevel({level:200, angle:-90, distance:150, time: 2}); break;
         }
     };
