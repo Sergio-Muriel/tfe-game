a088752f0f88e788cc5f920c41ac822e7c045376
Sergio Muriel <sergio.muriel@quiksilver.eu>
2017-01-13 20:54:36 +0100

    level3

--- js/items/stick.js
+++ js/items/stick.js
@@ -12,7 +12,7 @@ var Stick = function(game, options)
     this.weapon_speed=0.5;
     this.weapon_range = 15;
     this.weapon_attack_damage = 10;
-    this.weapon_hit_chance = 0.9;
+    this.weapon_hit_chance = 1.0;
 
     this.weapon_defense = 0.5;
     this.increase_life_value = 10;
