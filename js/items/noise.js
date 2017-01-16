9790ac4724828a37869a5326fba397f3a69ee273
Sergio Muriel <tfeserver@gmail.com>
2017-01-16 12:47:11 +0100

    noise selection display

--- js/items/noise.js
+++ js/items/noise.js
@@ -10,6 +10,12 @@ Noise.prototype.constructor = Noise;
 Noise.prototype.use_callback = function()
 {
     console.log('make some noise!');
-    play_multiple(game.assets.perso_noise_sound);
+    document.querySelector('.bone_attachment.noise').classList.add('selected');
+    window.setTimeout(this.stop_selection.bind(this), 500);
+    game.assets.perso_noise_sound.play();
+};
+Noise.prototype.stop_selection = function()
+{
+    document.querySelector('.bone_attachment.noise').classList.remove('selected');
 };
 
