65d9e79047dcf20f1f87ecfd84417fa1a56c9505
Sergio Muriel <tfeserver@gmail.com>
2017-02-16 17:47:00 +0100

    frozen expedition

--- js/labels.js
+++ js/labels.js
@@ -7,7 +7,7 @@ Game.prototype.labels =
     },
     'en':
     {
-        'game_name': 'The Frozen Experience',
+        'game_name': 'The Frozen Expedition',
         'life': 'Life',
         'loading_assets': 'Loading game data',
         'loading_level': 'Loading level',
